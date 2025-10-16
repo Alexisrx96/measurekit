fa96f7b1bcfee4da3324223280b764f070731646
Irvin Torres <irvinrx1996@hotmail.com>
2025-10-16 03:36:22 -0600

    feat: Move exceptions.py to domain layer

--- measurekit/domain/exceptions.py
+++ measurekit/domain/exceptions.py
@@ -11,7 +11,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from measurekit.measurement.units import CompoundUnit
+    from measurekit.domain.measurement.units import CompoundUnit
 
 
 class MeasureKitError(Exception):
